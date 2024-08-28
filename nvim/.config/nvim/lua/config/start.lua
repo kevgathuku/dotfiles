4643de81f062623b263ade61383984d799c2d265
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2024-08-29 01:07:52 +0200

    nvim: start porting vim settings

--- nvim/.config/nvim/lua/config/start.lua
+++ nvim/.config/nvim/lua/config/start.lua
@@ -2,6 +2,23 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set encoding=utf-8")
+vim.cmd("set showcmd")
+vim.cmd("set ruler")
+vim.cmd("set laststatus=2")
+vim.cmd("set cursorline")
+vim.cmd("set autoread")
+vim.cmd("set mouse=a")
+vim.cmd("set backspace=2")
+vim.cmd("set shell=zsh")
+vim.cmd("set rnu")
+vim.cmd("")
+vim.cmd("")
+vim.cmd("")
+vim.cmd("")
+vim.cmd("")
+vim.cmd("")
+
 
 vim.g.mapleader = " "
 
