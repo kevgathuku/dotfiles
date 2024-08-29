234c96990dc17b755e06cabd9eac00e64ed752e8
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2024-08-29 13:04:18 +0200

    nvim: autoindent with treesitter 🥳

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,13 +3,13 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-            local config = require("nvim-treesitter.configs")
-                  config.setup({
-                    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "ruby" },
-                    auto_install = true,
-                    highlight = { enable = true },
-                    indent = { enable = true },
-                  })
-            end
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "ruby" },
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
   }
 }
