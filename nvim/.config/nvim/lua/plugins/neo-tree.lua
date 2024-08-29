3fa011b08dd3e9e938200e2a65bf8e83f36b32bc
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2024-08-29 14:10:00 +0200

    nvim: neotree setup
    Add config to show dotfiles

---
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,21 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      filesystem = 
+        {
+          filtered_items = {
+            hide_dotfiles = false,
+          }
+        }
+    })
+    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal right<CR>", {})
+    vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+  end,
+}
