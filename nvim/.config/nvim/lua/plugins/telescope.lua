3708817f79f35933f091a3465cccfdc5bef065d4
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2024-08-29 12:29:37 +0200

    nvim: telescope can now find dotfiles

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+      vim.keymap.set('n', '<C-p>', '<cmd>Telescope find_files hidden=true<cr>', {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     end,
 }
