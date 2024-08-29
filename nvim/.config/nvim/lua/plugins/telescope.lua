2a2ab3f8da7d15fa2c2950cdbf2647934485ab7b
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2024-08-29 15:17:57 +0200

    nvim: lsp confit for lua

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,15 @@
 return {
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+  },
+  {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local builtin = require("telescope.builtin")
       vim.keymap.set('n', '<C-p>', '<cmd>Telescope find_files hidden=true<cr>', {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+      require("telescope").load_extension("ui-select")
     end,
-}
+  }}
