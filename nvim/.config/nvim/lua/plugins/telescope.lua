ee0114854ccc07c195086f7d2ba1aa5c6d69f04c
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2024-08-29 17:11:27 +0200

    nvim: Ensure telescope doesn't search gitignored files

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -6,9 +6,35 @@ return {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
+      local telescope = require("telescope")
       local builtin = require("telescope.builtin")
+      local telescopeConfig = require("telescope.config")
+      -- Clone the default Telescope configuration
+      local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+      -- I want to search in hidden/dot files.
+      table.insert(vimgrep_arguments, "--hidden")
+      -- I don't want to search in the `.git` directory.
+      table.insert(vimgrep_arguments, "--glob")
+      table.insert(vimgrep_arguments, "!**/.git/*")
+      --
       vim.keymap.set('n', '<C-p>', '<cmd>Telescope find_files hidden=true<cr>', {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+      telescope.setup({
+        defaults = {
+          -- `hidden = true` is not supported in text grep commands.
+          vimgrep_arguments = vimgrep_arguments,
+        },
+        pickers = {
+          find_files = {
+            -- `hidden = true` will still show the inside of `.git/` as it's not
+            --                   `.gitignore`d.
+            find_command = { "rg", "--files", "--hidden", "--glob",
+              "!**/.git/*" },
+          },
+        },
+      })
 
       require("telescope").load_extension("ui-select")
     end,
