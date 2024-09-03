5532e6fc242cb34ac9e98f5b139de5b93b57ab35
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2024-09-03 12:14:42 +0200

    nvim: add eslint config

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,22 +9,24 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
-      require("mason-lspconfig").setup {
-        ensure_installed = { "lua_ls" },
-      }
-    end
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "eslint" },
+      })
+    end,
   },
   {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
+              lspconfig.lua_ls.setup({})
+
+      lspconfig.eslint.setup({})
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end
-  }
+    end,
+  },
 }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,6 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "markdown", "markdown_inline", "ruby", "rust" },
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
