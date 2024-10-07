b0249a91c428dd5ee6146447263e55811e7113d5
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2024-10-07 22:42:58 +0200

    Format JS with prettier

--- nvim/.config/nvim/lua/plugins/formatter.lua
+++ nvim/.config/nvim/lua/plugins/formatter.lua
@@ -35,6 +35,19 @@ return {
             }
           end,
         },
+        javascript = {
+          function()
+            return {
+              exe = "prettier",
+              args = {
+                "--stdin-filepath",
+                util.escape_path(util.get_current_buffer_file_path()),
+              },
+              stdin = true,
+              try_node_modules = true,
+            }
+          end,
+        },
         ["*"] = {
           require("formatter.filetypes.any").remove_trailing_whitespace,
         },
