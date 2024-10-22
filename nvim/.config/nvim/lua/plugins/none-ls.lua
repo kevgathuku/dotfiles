1f7eba7f72872b1c1b0c49ab9590b9d5bba13921
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2024-10-22 11:18:49 +0200

    nvim: Enable inline rubocop diagnostics

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -7,6 +7,9 @@ return {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.diagnostics.erb_lint,
+        null_ls.builtins.diagnostics.rubocop.with {
+          command = { "/home/kevingathuku/workspace/kantox-flow/exe/rubocop" },
+        },
       },
     })
 
