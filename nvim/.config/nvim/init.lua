80af47925481c03b132b19af05fbfd386e2af9a1
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2024-10-22 15:07:22 +0200

    nvim: add shortcut to go to tag in new tab

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,4 +3,6 @@ vim.cmd('set runtimepath^=~/.vim runtimepath+=~/.vim/after')
 vim.o.packpath = vim.o.runtimepath
 vim.cmd('source ~/.vimrc')
 
+vim.api.nvim_set_keymap('n', '<leader>gt', ':tab tag <C-R><C-W><CR>', { noremap = true, silent = true })
+
 require("config.lazy")
