d0c8d2af0894e6c7ca09fa0d3556c84bd5e561e7
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2025-05-13 13:42:21 +0200

    Theming: Ensure lualine icons are shown in alacritty terminal
    - Update alacritty themes too

--- nvim/.config/nvim/lua/plugins/themes.lua
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -1,16 +1,29 @@
 return {
   {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup({
+        options = {
+          theme = 'gruvbox-material'
+          -- theme = 'auto'
+        }
+      })
+    end
+  },
+  {
     'maxmx03/solarized.nvim',
     lazy = false,
     priority = 1000,
     opts = {
-       variant = 'spring', -- "spring" | "summer" | "autumn" | "winter"
+      variant = 'spring', -- "spring" | "summer" | "autumn" | "winter"
     },
     config = function(_, opts)
       vim.o.termguicolors = true
       vim.o.background = 'light'
       require('solarized').setup(opts)
-      vim.cmd.colorscheme 'solarized'
+      -- vim.cmd.colorscheme 'solarized'
+      vim.cmd.colorscheme 'retrobox'
     end,
   },
 }
