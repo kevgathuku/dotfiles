0c8b31e50891f8ce0ff4ad227914eea80f4f9cc9
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2025-10-10 22:52:24 +0200

    nvim: Add custom rspec config

--- nvim/.config/nvim/lua/plugins/testing.lua
+++ nvim/.config/nvim/lua/plugins/testing.lua
@@ -5,7 +5,14 @@ return {
   },
   config = function()
     -- vim.g['test#strategy'] = 'neovim'
-    vim.g['test#strategy'] = 'vimux'
+    vim.g["test#strategy"] = "vimux"
+    -- Use Docker container for RSpec
+    vim.g["test#ruby#rspec#executable"] = "docker exec -it kantox-flow-app-1 bundle exec rspec"
+
+    -- Add options for the RSpec runner
+    vim.g["test#ruby#rspec#options"] = {
+      file = "--format documentation",
+    }
   end,
   cmd = {
     "TestNearest",
