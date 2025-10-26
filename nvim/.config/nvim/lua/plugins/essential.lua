1257ebc6d7e4c9e3329df8a9b8632efdb8930f72
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2025-10-26 10:08:46 +0100

    vim: install ragtag

--- nvim/.config/nvim/lua/plugins/essential.lua
+++ nvim/.config/nvim/lua/plugins/essential.lua
@@ -2,6 +2,7 @@ return {
   { "tpope/vim-commentary" },
   { "tpope/vim-unimpaired" },
   { "tpope/vim-fugitive" },
+  { "tpope/vim-ragtag" },
   { "junegunn/gv.vim" },
   { "tpope/vim-rails" },
   { "tpope/vim-rhubarb" },
