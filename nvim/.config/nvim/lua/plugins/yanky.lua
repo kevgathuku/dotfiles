5f6b1af1565d91d3cf84598a9adc0e4263a30469
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2025-10-29 18:43:57 +0100

    nvim: Fix marks and cut commands

--- nvim/.config/nvim/lua/plugins/yanky.lua
+++ nvim/.config/nvim/lua/plugins/yanky.lua
@@ -16,15 +16,31 @@ return {
       { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
       { "<leader>p", "<Plug>(YankyCycleForward)", desc = "Cycle forward through yank history" },
       { "<leader>n", "<Plug>(YankyCycleBackward)", desc = "Cycle backward through yank history" },
+      {
+        "gm",
+        function()
+          -- Wait for next key and feed "m{key}" to Vim
+          local char = vim.fn.getcharstr()
+          vim.cmd("normal! m" .. char)
+        end,
+        mode = "n",
+        desc = "Set mark (was 'm')",
+      },
     },
   },
-
-  -- replacement for cutlass.nvim
   {
     "gbprod/cutlass.nvim",
     opts = {
-      cut_key = "m", -- same as your old config
+      cut_key = "m", -- Use `m` for motion-aware cut
+      override_del = true, -- Make `x` use the black-hole register by default
+      exclude = { "ns", "nS" }, -- Optional: exclude substitute commands
     },
+    config = function(_, opts)
+      require("cutlass").setup(opts)
+
+      -- Optional: make delete/change not yank
+      vim.keymap.set({ "n", "x" }, "d", '"_d', { desc = "Delete without yanking" })
+      vim.keymap.set({ "n", "x" }, "c", '"_c', { desc = "Change without yanking" })
+    end,
   },
 }
-
