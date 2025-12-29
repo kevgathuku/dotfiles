0baacb31587104d2724314a7059d698e7e33947e
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2025-12-29 12:07:32 +0100

    nvim: clojure setup

---
+++ nvim/.config/nvim/lua/plugins/clojure.lua
@@ -0,0 +1,32 @@
+return {
+  { "guns/vim-sexp" },
+  { "tpope/vim-sexp-mappings-for-regular-people" },
+  { "tpope/vim-fireplace" },
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    event = "BufReadPost",
+    config = function()
+      local rainbow_delimiters = require("rainbow-delimiters")
+
+      vim.g.rainbow_delimiters = {
+        strategy = {
+          [""] = rainbow_delimiters.strategy["global"],
+          vim = rainbow_delimiters.strategy["local"],
+        },
+        query = {
+          [""] = "rainbow-delimiters",
+          lua = "rainbow-blocks",
+        },
+        highlight = {
+          "RainbowDelimiterRed",
+          "RainbowDelimiterYellow",
+          "RainbowDelimiterBlue",
+          "RainbowDelimiterOrange",
+          "RainbowDelimiterGreen",
+          "RainbowDelimiterViolet",
+          "RainbowDelimiterCyan",
+        },
+      }
+    end,
+  },
+}
