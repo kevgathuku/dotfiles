ff4c6ef7ff2994efaa8f78093fa708dd9559a470
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2026-01-22 17:58:17 +0100

    nvim: install alablaster theme

--- nvim/.config/nvim/lua/plugins/essential.lua
+++ nvim/.config/nvim/lua/plugins/essential.lua
@@ -10,5 +10,6 @@ return {
   { "tpope/vim-projectionist" },
   { "vim-ruby/vim-ruby" },
   { "yegappan/mru" },
+  { "p00f/alabaster.nvim" },
   { "tek/vim-textobj-ruby", dependencies = { "kana/vim-textobj-user" } },
 }
