997099328605335fbd187b91b28bc645368d8b14
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2026-01-22 17:58:36 +0100

    nvim: remove unused shortcuts

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,12 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Move to next tab with Tab
-vim.keymap.set("n", "<Tab>", "gt", { desc = "Next Tab" })
-
--- Move to previous tab with Shift-Tab
-vim.keymap.set("n", "<S-Tab>", "gT", { desc = "Previous Tab" })
-
 -- Colorscheme picker
 vim.api.nvim_create_user_command("Colors", function()
   require("telescope.builtin").colorscheme({
