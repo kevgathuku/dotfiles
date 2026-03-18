334d34727386b59980c2a847cbe9d2dc4a78e738
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2026-03-18 12:20:38 +0100

    themes config

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -5,3 +5,6 @@ vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
 
 -- Disable automatic clipboard sync - yanking in vim stays in vim only
 vim.opt.clipboard = ""
+
+-- Use light background for alabaster theme
+vim.o.background = "light"
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -18,7 +18,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "solarized",
+      colorscheme = "alabaster",
     },
   },
 }
